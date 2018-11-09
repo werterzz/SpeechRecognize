44dc427b4d35b3db36cb4068b5bcf1fe6053fee2
ASUS <ASUS@DESKTOP-1D7DVPC>
2018-11-10 01:29:11 +0700

    add file Speech

---
+++ VoiceFormsApp2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace VoiceFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.richTextBox1.Location = new System.Drawing.Point(47, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(305, 166);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "log";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(93, 241);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(75, 23);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "button1";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(241, 241);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(75, 23);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "button2";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 317);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ VoiceFormsApp2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace VoiceFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "say hello", "print my name", "kill wolf" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+
+        }
+
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "say hello":
+                    MessageBox.Show("hello Top");
+                    break;
+                case "print my name":
+                    richTextBox1.Text += "\nTop";
+                    break;
+                case "kill wolf":
+                    richTextBox1.Text += "\nwolf has been kill";
+                    break;
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+        }
+    }
+}
